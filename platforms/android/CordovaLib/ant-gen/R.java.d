7a40814d5dd7aff6d64938e2968bd73368a4d31e
Erik Reppel <erikreppel@gmail.com>
2014-08-21 07:48:09 -0700

    Initial Commit

---
+++ platforms/android/CordovaLib/ant-gen/R.java.d
@@ -0,0 +1,1 @@
+ : C:\Users\Erik\Dev\Projects\BarbellToolbox\platforms\android\CordovaLib\ant-build\AndroidManifest.xml \
---
+++ platforms/android/ant-gen/com/erikreppel/barbelltoolbox/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.erikreppel.barbelltoolbox;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
