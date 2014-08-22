fbcb4ca6eb1fba880d38e4c1179c912aa009cb19
Erik Reppel <erikreppel@gmail.com>
2014-08-21 17:36:24 -0700

    change to bootstrap 3.0

---
+++ platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\Erik\Dev\Projects\BarbellToolbox\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
+C:\Users\Erik\Dev\Projects\BarbellToolbox\platforms\android\CordovaLib\AndroidManifest.xml \
---
+++ platforms/android/ant-build/BarbellToolbox-release-unsigned.apk.d
@@ -0,0 +1,4 @@
+c:\Users\Erik\Dev\Projects\BarbellToolbox\platforms\android\ant-build\BarbellToolbox-release-unsigned.apk : \
+c:\Users\Erik\Dev\Projects\BarbellToolbox\platforms\android\ant-build\BarbellToolbox.ap_ \
+c:\Users\Erik\Dev\Projects\BarbellToolbox\platforms\android\ant-build\classes.dex \
+C:\Users\Erik\Dev\Projects\BarbellToolbox\platforms\android\CordovaLib\ant-build\classes.jar \
